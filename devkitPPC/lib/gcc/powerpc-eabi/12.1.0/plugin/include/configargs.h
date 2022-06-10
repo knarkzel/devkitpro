c7379bbbeb2dc4b07bdb707c100c66a6662ca597
Knarkzel <knarkzel@gmail.com>
2022-06-10 03:46:10 +0200

    Initial commit

---
+++ devkitPPC/lib/gcc/powerpc-eabi/12.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../gcc-12.1.0/configure --enable-languages=c,c++,objc,lto --enable-lto --with-cpu=750 --disable-nls --disable-shared --enable-threads=dkp --disable-multilib --disable-win32-registry --disable-libstdcxx-pch --disable-libstdcxx-verbose --enable-libstdcxx-time=yes --enable-libstdcxx-filesystem-ts --disable-tm-clone-registry --disable-__cxa_atexit --enable-cxx-flags='-ffunction-sections -fdata-sections' --target=powerpc-eabi --with-newlib --with-headers=../../newlib-4.2.0.20211231/newlib/libc/include --prefix=/home/davem/projects/devkitpro/tool-packages/devkitPPC/src/build/x86_64-linux-gnu/devkitPPC --with-bugurl=https://github.com/devkitpro/buildscripts/issues --with-pkgversion='devkitPPC release 41' --build=x86_64-unknown-linux-gnu --host=x86_64-unknown-linux-gnu --with-gmp= --with-mpfr= --with-mpc= --with-isl= --with-zstd= --with-gmp=/opt/devkitpro/crosstools/x86_64-linux-gnu --with-mpfr=/opt/devkitpro/crosstools/x86_64-linux-gnu --with-mpc=/opt/devkitpro/crosstools/x86_64-linux-gnu --with-isl=/opt/devkitpro/crosstools/x86_64-linux-gnu --with-zstd=/opt/devkitpro/crosstools/x86_64-linux-gnu 'CFLAGS_FOR_TARGET=-O2 -ffunction-sections -fdata-sections'";
+static const char thread_model[] = "dkp";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "750" } };
